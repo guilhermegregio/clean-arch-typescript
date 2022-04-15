ca19cf58b9c9c5ec4c6f1bc6b23485fc95cc0216
Guilherme Mangabeira Gregio <guilherme.gregio@creditas.com.br>
2022-04-15 20:33:23 -0300

    test: refactor to split expect

--- src/data/usecases/load-purchases/local-save-purchases.test.ts
+++ src/data/usecases/load-purchases/local-save-purchases.test.ts
@@ -11,60 +11,125 @@ type SutTypes = {
 }
 const makeSut = (timestamp = new Date()): SutTypes => {
   const cacheStore = new CacheStoreSpy()
+  //SUT System under test
   const sut = new LocalLoadPurchases(cacheStore, timestamp)
 
   return {sut, cacheStore}
 }
 
-describe('LocalLoadPurchases', () => {
+describe('LocalLoadPurchases.save()', () => {
   test('does not delete or insert cache on init', () => {
     const {cacheStore} = makeSut()
 
     expect(cacheStore.actions).toEqual([])
   })
 
-  test('does not insert new cache when delete fails', async () => {
-    const {cacheStore, sut} = makeSut()
-    cacheStore.simulateDeleteError()
-    const purchases = mockPurchases()
+  describe('when delete succeeds', () => {
+    let instance: SutTypes, timestamp: Date
 
-    const promise = sut.save(purchases)
+    beforeEach(() => {
+      timestamp = new Date()
+      instance = makeSut(timestamp)
+    })
 
-    await expect(promise).rejects.toThrow()
-    expect(cacheStore.actions).toEqual([CacheStoreSpyActions.delete])
-  })
+    test('calls delete and insert', async () => {
+      const {cacheStore, sut} = instance
 
-  test('inserts new cache when delete succeeds', async () => {
-    const timestamp = new Date()
-    const {cacheStore, sut} = makeSut(timestamp)
-    const purchases = mockPurchases()
+      await sut.save(mockPurchases())
 
-    const promise = sut.save(purchases)
+      expect(cacheStore.actions).toEqual([
+        CacheStoreSpyActions.delete,
+        CacheStoreSpyActions.insert,
+      ])
+    })
 
-    await expect(promise).resolves.toBeFalsy()
-    expect(cacheStore.actions).toEqual([
-      CacheStoreSpyActions.delete,
-      CacheStoreSpyActions.insert,
-    ])
-    expect(cacheStore.deleteKey).toBe('purchases')
-    expect(cacheStore.insertKey).toBe('purchases')
-    expect(cacheStore.insertValues).toEqual({
-      timestamp,
-      value: purchases,
+    test('calls delete with correct key', async () => {
+      const {cacheStore, sut} = instance
+
+      await sut.save(mockPurchases())
+
+      expect(cacheStore.deleteKey).toBe('purchases')
+    })
+
+    test('calls insert with correct key', async () => {
+      const {cacheStore, sut} = instance
+
+      await sut.save(mockPurchases())
+
+      expect(cacheStore.insertKey).toBe('purchases')
+    })
+
+    test('inserts new cache', async () => {
+      const {cacheStore, sut} = instance
+      const purchases = mockPurchases()
+
+      await sut.save(purchases)
+
+      expect(cacheStore.insertValues).toEqual({
+        timestamp,
+        value: purchases,
+      })
+    })
+
+    test('inserts new cache without error', async () => {
+      const {sut} = instance
+
+      const promise = sut.save(mockPurchases())
+
+      await expect(promise).resolves.toBeFalsy()
     })
   })
 
-  test('throws error when insert fails', async () => {
-    const {cacheStore, sut} = makeSut()
-    cacheStore.simulateInsertError()
-    const purchases = mockPurchases()
+  describe('when delete fails', () => {
+    let instance: SutTypes
 
-    const promise = sut.save(purchases)
+    beforeEach(() => {
+      instance = makeSut()
+      instance.cacheStore.simulateDeleteError()
+    })
 
-    await expect(promise).rejects.toThrow()
-    expect(cacheStore.actions).toEqual([
-      CacheStoreSpyActions.delete,
-      CacheStoreSpyActions.insert,
-    ])
+    test('throws error', async () => {
+      const {sut} = instance
+
+      const promise = sut.save(mockPurchases())
+
+      await expect(promise).rejects.toThrow()
+    })
+
+    test('does not insert new cache', done => {
+      const {cacheStore, sut} = instance
+
+      sut.save(mockPurchases()).catch(() => done())
+
+      expect(cacheStore.actions).toEqual([CacheStoreSpyActions.delete])
+    })
+  })
+
+  describe('when insert fails', () => {
+    let instance: SutTypes
+
+    beforeEach(() => {
+      instance = makeSut()
+      instance.cacheStore.simulateInsertError()
+    })
+
+    test('throws error', async () => {
+      const {sut} = instance
+
+      const promise = sut.save(mockPurchases())
+
+      await expect(promise).rejects.toThrow()
+    })
+
+    test('calls delete and insert', done => {
+      const {cacheStore, sut} = instance
+
+      sut.save(mockPurchases()).catch(() => done())
+
+      expect(cacheStore.actions).toEqual([
+        CacheStoreSpyActions.delete,
+        CacheStoreSpyActions.insert,
+      ])
+    })
   })
 })
